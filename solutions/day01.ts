fe0c398212125e7251876cecb77a1d81839b2d4b
Alex Munro <alexmunro91@gmail.com>
2022-12-02 03:20:41 +0000

    Day 1 part two

--- solutions/day01.ts
+++ solutions/day01.ts
@@ -10,18 +10,28 @@ function parseElfFoods(rawInput: string): number[][] {
 }
 
 function caloriesPerElf(elfFoods: number[][]): number[] {
-    return elfFoods.map((elf) => {
-        return elf.reduce((sum, nextFood) => sum + nextFood, 0)
-    });
+    return elfFoods.map((elf) => sum(elf));
 }
 
 function mostCaloriesOnAnyElf(caloriesPerElf: number[]): number {
     return Math.max(...caloriesPerElf);
 }
 
+function topThree(caloriesPerElf: number[]): number[] {
+    return caloriesPerElf.sort((a, b) => a - b).slice(-3);
+}
+
+function sum(numbers: number[]): number {
+    return numbers.reduce((sum, n) => sum + n);
+}
 
 function partOne(): string {
     return mostCaloriesOnAnyElf(caloriesPerElf(parseElfFoods(file))).toString();
 }
 
+function partTwo(): string {
+    return sum(topThree(caloriesPerElf(parseElfFoods(file)))).toString();
+}
+
 console.log("The answer to part one is " + partOne());
+console.log("The answer to part two is " + partTwo());
