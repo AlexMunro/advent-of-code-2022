ae02cd06cabe8f0f0f1ecc8887c3e733e19c3313
Alex Munro <alexmunro91@gmail.com>
2023-09-10 15:23:04 +0100

    Day 6 part two solution

--- solutions/day06.ts
+++ solutions/day06.ts
@@ -1,16 +1,31 @@
 import * as fs from "fs";
 
-export function partOne(signal: string): number {
-  for (let i = 4; i < signal.length; i++) {
-    if (new Set(signal.slice(i - 4, i)).size == 4) {
+function firstDistinctNCharsIndex(signal: string, n: number): number {
+  let i = n;
+
+  while (i < signal.length) {
+    const distinctCharsInChunk = new Set(signal.slice(i - n, i)).size;
+
+    if (distinctCharsInChunk == n) {
       return i;
+    } else {
+      i += n - distinctCharsInChunk; // Skip ahead as far as we know we can
     }
   }
 
-  throw Error("Signal does not have start-of-packet marker");
+  throw Error(`Unable to find distinct sequence of ${n} chars in ${signal}`);
+}
+
+export function partOne(signal: string): number {
+  return firstDistinctNCharsIndex(signal, 4);
+}
+
+export function partTwo(signal: string): number {
+  return firstDistinctNCharsIndex(signal, 14);
 }
 
 if (require.main === module) {
   const input: string = fs.readFileSync("inputs/day06.txt", "utf-8").trim();
   console.log(partOne(input));
+  console.log(partTwo(input));
 }
--- tests/day06.test.ts
+++ tests/day06.test.ts
@@ -1,4 +1,4 @@
-import { partOne } from "../solutions/day06";
+import { partOne, partTwo } from "../solutions/day06";
 
 describe("Day 06", () => {
   describe("partOne", () => {
@@ -8,6 +8,16 @@ describe("Day 06", () => {
       expect(partOne("nppdvjthqldpwncqszvftbrmjlhg")).toEqual(6);
       expect(partOne("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toEqual(10);
       expect(partOne("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(11);
+    });
+  });
+
+  describe("partTwo", () => {
+    it("returns the position of the first start-of-message marker", () => {
+      expect(partTwo("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(19);
+      expect(partTwo("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(23);
+      expect(partTwo("nppdvjthqldpwncqszvftbrmjlhg")).toEqual(23);
+      expect(partTwo("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toEqual(29);
+      expect(partTwo("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(26);
     });
   });
 });
