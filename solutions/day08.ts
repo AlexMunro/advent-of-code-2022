322831fc304e0582da5ed1048a8647617fc5ae49
Alex Munro <alexmunro91@gmail.com>
2023-11-20 01:26:54 +0000

    Day 8 part two solution

--- solutions/day08.ts
+++ solutions/day08.ts
@@ -1,30 +1,48 @@
 import * as fs from "fs";
 import runningAsMain from "../utils/runningAsMain";
 
+function treesLeftOf(
+  row: number,
+  column: number,
+  forest: number[][],
+): number[] {
+  return forest[row].slice(0, column);
+}
+
+function treesRightOf(
+  row: number,
+  column: number,
+  forest: number[][],
+): number[] {
+  return forest[row].slice(column + 1);
+}
+
+function treesAbove(row: number, column: number, forest: number[][]): number[] {
+  return forest.slice(0, row).map((r) => r[column]);
+}
+
+function treesBelow(row: number, column: number, forest: number[][]): number[] {
+  return forest.slice(row + 1).map((r) => r[column]);
+}
+
 function treeIsVisible(
   row: number,
   column: number,
   forest: number[][],
 ): boolean {
-  // Visible from the left
-  const treesToTheLeft = forest[row].slice(0, column);
-  if (forest[row][column] > Math.max(...treesToTheLeft)) {
+  if (forest[row][column] > Math.max(...treesLeftOf(row, column, forest))) {
     return true;
   }
 
-  const treesToTheRight = forest[row].slice(column + 1);
-  if (forest[row][column] > Math.max(...treesToTheRight)) {
+  if (forest[row][column] > Math.max(...treesRightOf(row, column, forest))) {
     return true;
   }
 
-  // Visible from the bottom
-  const treesAbove = forest.slice(0, row).map((r) => r[column]);
-  if (forest[row][column] > Math.max(...treesAbove)) {
+  if (forest[row][column] > Math.max(...treesAbove(row, column, forest))) {
     return true;
   }
 
-  const treesBelow = forest.slice(row + 1).map((r) => r[column]);
-  if (forest[row][column] > Math.max(...treesBelow)) {
+  if (forest[row][column] > Math.max(...treesBelow(row, column, forest))) {
     return true;
   }
 
@@ -45,10 +63,58 @@ function interiorVisibleTreeCount(forest: number[][]): number {
   return visibleCount;
 }
 
+function canSeeFirst(treeSize: number, otherTrees: number[]): number {
+  let seenTrees = 0;
+  for (const nextTree of otherTrees) {
+    seenTrees++;
+    if (nextTree >= treeSize) {
+      break;
+    }
+  }
+  return seenTrees;
+}
+
+function scenicScore(row: number, column: number, forest: number[][]) {
+  const treeSize = forest[row][column];
+
+  const scenicLeft = canSeeFirst(
+    treeSize,
+    treesLeftOf(row, column, forest).reverse(),
+  );
+
+  const scenicRight = canSeeFirst(treeSize, treesRightOf(row, column, forest));
+
+  const scenicUp = canSeeFirst(
+    treeSize,
+    treesAbove(row, column, forest).reverse(),
+  );
+
+  const scenicDown = canSeeFirst(
+    treeSize, treesBelow(row, column, forest)
+  );
+
+  return scenicLeft * scenicRight * scenicUp * scenicDown;
+}
+
 export function partOne(input: number[][]): number {
   return (
     input.length * 2 + input[0].length * 2 - 4 + interiorVisibleTreeCount(input)
   );
+}
+
+export function partTwo(input: number[][]): number {
+  let maxScenicScore = 0;
+
+  for (let row = 1; row < input.length - 1; row++) {
+    for (let column = 1; column < input[0].length - 1; column++) {
+      const currentScenicScore = scenicScore(row, column, input);
+      if (currentScenicScore > maxScenicScore) {
+        maxScenicScore = currentScenicScore;
+      }
+    }
+  }
+
+  return maxScenicScore;
 }
 
 if (runningAsMain()) {
@@ -59,4 +125,5 @@ if (runningAsMain()) {
     .map((s) => s.split("").map((char) => Number.parseInt(char)));
 
   console.log("The answer to part one is " + partOne(input));
+  console.log("The answer to part two is " + partTwo(input));
 }
--- tests/day08.test.ts
+++ tests/day08.test.ts
@@ -1,17 +1,23 @@
-import { partOne } from "../solutions/day08";
+import { partOne, partTwo } from "../solutions/day08";
 
 describe("Day08", () => {
-  describe("partOne", () => {
-    const input = [
-      [3, 0, 3, 7, 3],
-      [2, 5, 5, 1, 2],
-      [6, 5, 3, 3, 2],
-      [3, 3, 5, 4, 9],
-      [3, 5, 3, 9, 0],
-    ];
+  const input = [
+    [3, 0, 3, 7, 3],
+    [2, 5, 5, 1, 2],
+    [6, 5, 3, 3, 2],
+    [3, 3, 5, 4, 9],
+    [3, 5, 3, 9, 0],
+  ];
 
+  describe("partOne", () => {
     it("counts the visible trees", () => {
       expect(partOne(input)).toBe(21);
+    });
+  });
+
+  describe("partTwo", () => {
+    it("finds the highest scenic score", () => {
+      expect(partTwo(input)).toBe(8);
     });
   });
 });
